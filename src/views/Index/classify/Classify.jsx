83400f7ef5966b9028d7b5f48daa6f1d8b7ba8c8
WKSS18 <w1455068403@163.com>
2020-05-08 13:02:48 +0800

    'update'

--- src/views/Index/classify/Classify.jsx
+++ src/views/Index/classify/Classify.jsx
@@ -42,34 +42,7 @@ class Classify extends Component {
                         </div>
 
                         <div className="rightswiper">
-                            <ul className="classifyRf">
-                            
-                                {
-                                    this.state.slideRflist.map((item) => {
-                                        return (
-                                            <>
-                                                <img id={'8888'+Math.floor(Math.random()*20000000)} key={'8888'+Math.floor(Math.random()*20000000)} className="classifyTit" style={{display:item.category_list[0].body.items ? 'block' :'none'}}  src={item.category_list[0].body.items ? item.category_list[0].body.items[0].img_url : ''} />
-                                                <li key={item.category_id}>
-                                                    {
-                                                        item.category_list.map((item)=>{
-                                                            return (
-                                                               <ul id={'55'+Math.floor(Math.random()*10000)} key={'55'+Math.floor(Math.random()*10000)}>
-                                                                    {/* <p id={Math.floor(Math.random()*1000000)} key={Math.floor(Math.random()*1000000)}>{item.body.category_name ? item.body.category_name : ''}</p>
-                                                                    <li id={Math.floor(Math.random()*10000222)} key={Math.floor(Math.random()*10000222)}>
-                                                                       
-                                                                    </li> */}
-                                                               </ul>
-                                                                
-                                                            )
-                                                        })
-                                                    }
-                                                </li>
-                                            </>
-
-                                        )
-                                    })
-                                }
-                            </ul>
+                          
                         </div>
 
                     </div>
@@ -81,7 +54,6 @@ class Classify extends Component {
 
     async getDatalist() {
         let result = await get('/api/classify');
-        console.log(result);
         let slideLflist = [];
         for (var i = 0; i < result.data.data.length; i++) {
             slideLflist.push({ category_name: result.data.data[i].category_name, category_id: result.data.data[i].category_id });
@@ -90,7 +62,6 @@ class Classify extends Component {
             slideLflist: slideLflist,
             slideRflist: result.data.data
         })
-        console.log(result.data.data[0].category_list[0].body.items[0].img_url)
     }
     componentDidMount() {
         this.getDatalist();
